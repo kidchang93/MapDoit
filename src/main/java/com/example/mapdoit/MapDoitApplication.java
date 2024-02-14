d7a0f45c181cc96a6969fffcfdf6d7592f71da14
kidchang93 <lck930721@gmail.com>
2024-02-14 13:15:19 +0900

    스프링부트와 지도 API 를 활용한 프로젝트 시작

---
+++ src/main/java/com/example/mapdoit/MapDoitApplication.java
@@ -0,0 +1,13 @@
+package com.example.mapdoit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapDoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapDoitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mapdoit/MapDoitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapdoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapDoitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
